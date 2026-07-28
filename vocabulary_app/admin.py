e6d55528de7ade4f5c8eab0a61e35d8a36e55f1c
Damian Poloczek <damianpoloczek@gmail.com>
2026-07-28 14:17:11 +0200

    admin panel vocabulary_app

--- vocabulary_app/admin.py
+++ vocabulary_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
 from vocabulary_app.models import Language
 from vocabulary_app.models import UserLanguages
-
+from django.forms import CheckboxSelectMultiple
+from django.db import models
 
 admin.site.register(Language)
-admin.site.register(UserLanguages)
+
+@admin.register(UserLanguages)
+class UserLanguagesAdmin(ModelAdmin):
+    formfield_overrides = {
+    models.ManyToManyField: {"widget": CheckboxSelectMultiple},
+    }
