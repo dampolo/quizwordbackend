9907bc6d84433eeea71e7bee4f3f324618f8c0c1
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-05 07:43:03 +0200

    TEMPLATES with static

--- quizword_core/settings.py
+++ quizword_core/settings.py
@@ -108,7 +108,9 @@ ROOT_URLCONF = 'quizword_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
