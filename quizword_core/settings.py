c561fc6b78c9373583b9082d582bcfda29d3a6bb
dampolo <damianpoloczek@damian-server.europe-west10-c.c.planar-ripsaw-471114-p0.internal>
2026-08-07 09:37:57 +0000

    chamge of dokcer-compose.yml

--- quizword_core/settings.py
+++ quizword_core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-2--ir7&37dm!$((nisv7&%)m7utmhj%2@^20so@wh1b=u7of#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'quiz-word.com'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'quiz-word.com',
                  '34.32.2.236']
 
 
