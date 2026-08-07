45629b723094d76e016507a328044a49fdd280b0
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-07 11:57:21 +0200

    change of settings

--- quizword_core/settings.py
+++ quizword_core/settings.py
@@ -57,8 +57,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -189,7 +189,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Where collectstatic will put files in production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
