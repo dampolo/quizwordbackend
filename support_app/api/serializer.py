b2d4163c8d0e4b6c8520b74825077a936c3315a1
Damian Poloczek <dpoloczek@criticalsoftware.com>
2026-09-15 08:24:34 +0200

    remove code in serializers

--- support_app/api/serializer.py
+++ support_app/api/serializer.py
@@ -3,11 +3,6 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-from auth_app.validators import CustomPasswordValidator
-
-User = get_user_model()
 
 class SupportSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
